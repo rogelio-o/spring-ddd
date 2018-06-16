b287aafa3d5f0d0eff99c0536b0e0d4702fcd13c
rogelio-o <yo@rogelioorts.com>
2018-06-16 20:08:03 +0200

    Adds Spring dependencies.

---
+++ spring-ddd-boot/src/main/java/com/rogelioorts/training/spring/ddd/MainApplication.java
@@ -0,0 +1,13 @@
+package com.rogelioorts.training.spring.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(MainApplication.class, args);
+  }
+
+}
---
+++ spring-ddd-infrastructure/src/main/java/com/rogelioorts/training/spring/ddd/ConfigFeign.java
@@ -0,0 +1,10 @@
+package com.rogelioorts.training.spring.ddd;
+
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@EnableFeignClients
+@Configuration
+public class ConfigFeign {
+	
+}
