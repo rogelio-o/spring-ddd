d9cd90396ec30b53a2864a240dbabd64be5d869f
rogelio-o <yo@rogelioorts.com>
2018-06-18 19:51:11 +0200

    Some fixes to infrastructure layer.

--- spring-ddd-infrastructure/src/main/java/com/rogelioorts/training/spring/ddd/entities/FeignMovie.java
+++ spring-ddd-infrastructure/src/main/java/com/rogelioorts/training/spring/ddd/entities/FeignMovie.java
@@ -1,15 +1,12 @@
 package com.rogelioorts.training.spring.ddd.entities;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class FeignMovie {
 
-	@EqualsAndHashCode.Include
 	private long id;
 	
 	private String tit;
